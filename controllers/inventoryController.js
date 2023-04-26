ef523824d5315d74aab713bbca96ec8cdb6a8e67
Sharkri <sharkdude955@gmail.com>
2023-04-26 00:59:10 -0500

    Add better error handling

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -30,7 +30,7 @@ exports.itemPage = asyncHandler(async (req, res, next) => {
 });
 
 exports.addItemFormGET = asyncHandler(async (req, res, next) => {
-  const allCategories = await Category.find().exec();
+  const allCategories = await Category.find({}, "name").exec();
 
   res.render("create-item", {
     title: "Create Item",
@@ -53,6 +53,19 @@ function convertToArray(value) {
   return value;
 }
 
+async function checkIfCategoriesExists(categories) {
+  const categoryCount = await Category.countDocuments({
+    _id: { $in: categories },
+  });
+
+  // if one or more categories were not found in the database
+  if (categoryCount !== categories.length) {
+    return Promise.reject();
+  }
+
+  return true;
+}
+
 exports.addItemFormPOST = [
   (req, res, next) => {
     req.body.categories = convertToArray(req.body.categories);
@@ -66,30 +79,50 @@ exports.addItemFormPOST = [
     .withMessage("Name must be specified."),
   body("description").isString(),
 
-  // Does not check if categories all exist. (could maybe do)
-  body("categories.*"),
+  body("categories")
+    .custom(checkIfCategoriesExists)
+    .withMessage(
+      "One or more categories do not exist. Please create them first."
+    ),
 
-  body("price").isNumeric({ min: 0 }),
-  body("numberInStock").isInt({ min: 0 }),
+  body("price").isFloat({ min: 0 }).withMessage("Please enter a valid price."),
+  body("numberInStock")
+    .isInt({ min: 0 })
+    .withMessage("Please enter a valid stock number"),
 
   asyncHandler(async (req, res, next) => {
     // Extract the validation errors from a request.
     const errors = validationResult(req);
-    console.log(req.body);
+
+    const { name, description, categories, price, numberInStock } = req.body;
+
+    const item = new Item({
+      name,
+      description,
+      categories,
+      price: { amount: price },
+      numberInStock,
+    });
 
     if (!errors.isEmpty()) {
-      res.status(422).json({ errors: errors.array() });
-    } else {
-      const { name, description, categories, price, numberInStock } = req.body;
+      const allCategories = await Category.find({}, "name").exec();
 
-      const item = new Item({
+      res.render("create-item", {
+        title: "Create Item",
         name,
         description,
-        categories,
-        price: { amount: price },
+        // mark all categories that user selected
+        categories: allCategories.map((category) =>
+          categories.includes(category._id.toString())
+            ? { name: category.name, _id: category._id, selected: "selected" }
+            : category
+        ),
+        price,
         numberInStock,
-      });
 
+        errors: errors.array(),
+      });
+    } else {
       await item.save();
       res.redirect(item.url);
     }
